13fc05a31ea96fdae9b7e9a09e463022514336d5
Sunmughan Swamy <sunmughaN@Gmail.com>
2021-11-28 15:18:07 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -150,
-      y: 350,
+      x: 100,
+      y: 300,
       rotation: -180,
       opacity: 0
     })
